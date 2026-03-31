b0397d36877487a0f9c9fa8c00954bd23d07a30a
khilou206 <mohammedkhilou13@gmail.com>
2026-03-31 22:24:55 +0100

    save my changes

--- my-backend/routes/api.php
+++ my-backend/routes/api.php
@@ -15,8 +15,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/personalitytest', [AuthController::class, 'personalitytest']);
 Route::post('/check-email', [AuthController::class, 'checkEmail']);
-Route::get('/user-profile/{id}', [AuthController::class, 'getProfile']);
-Route::get('/user-posts/{id}', [PostController::class, 'getUserPosts']);
 
 Route::middleware('auth:sanctum')->post('/delete-account', [AuthController::class, 'deleteAccount']);
 Route::middleware('auth:sanctum')->post('/change-password', [AuthController::class, 'changePassword']);
@@ -36,7 +34,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/posts/{id}/toggle-like', [PostController::class, 'toggleLike']);
     Route::get('/my-liked-posts', [PostController::class, 'getLikedPosts']);
 
-
+   // بدل {post} بـ {id} باش تطابق مع الـ Axios اللي عندك
 Route::get('/posts/{id}/comments', [CommentController::class, 'index']);
 Route::post('/posts/{id}/comments', [CommentController::class, 'store']);
     Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
@@ -60,13 +58,11 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 // chat 
 Route::middleware('auth:sanctum')->group(function () {
-Route::get('/my-current-salon', [GroupController::class, 'getPendingGroup']);
-Route::get('/my-completed-groups', [GroupController::class, 'getMyGroups']);
+    Route::get('/my-completed-groups', [GroupController::class, 'getMyGroups']);
 Route::get('/groups/{groupId}/messages', [MessageController::class, 'fetchMessages']);
 Route::get('/groups/pending', [GroupController::class, 'getPendingGroup']);
-Route::post('/groups/random-join', [GroupController::class, 'joinRandomOrCreate']);
+    Route::post('/groups/random-join', [GroupController::class, 'joinRandomOrCreate']);
 Route::post('/groups/{groupId}/messages', [MessageController::class, 'store']);
 });
-
-
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
