392b3850076c14e15908095b77bb553ca1a2c90e
mohammed <mohammedkhilou13@gmail.com>
2026-06-09 18:10:49 +0100

    حفظ التعديلات المحلية

--- Model/app.py
+++ Model/app.py
@@ -9,10 +9,7 @@ try:
     with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
         print(type(model))
-        print(model[0])
-        print(model[1])
-        print(model[2])
-        print(model[3])
+      
 
         
 except Exception as e:
